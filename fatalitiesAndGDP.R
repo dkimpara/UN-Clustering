49ef6e42c1737c92e9a8b525a4607bb15bc59d2a
dkimpara <dhammakimpara@gmail.com>
2017-09-07 19:48:50 +0700

    redid axes

--- fatalitiesAndGDP.R
+++ fatalitiesAndGDP.R
@@ -3,15 +3,15 @@ eval(parse("helperFunctions.r"))
 source <- read.csv("Workers_Fatality_VS_GDP.csv")
 
 labels <- source[[1]]
-df.fatal <- data.frame(normalize(source$Workers_Fatality_per_100000_per_year),
-                       normalize(sqrt(source$GDP_per_cap)))
-source <- data.frame(source$Workers_Fatality_per_100000_per_year,
-                       source$GDP_per_cap)
+df.fatal <- data.frame(normalize(sqrt(source$GDP_per_cap)), 
+                       normalize(source$Workers_Fatality_per_100000_per_year))
+source <- data.frame(source$GDP_per_cap,
+                     source$Workers_Fatality_per_100000_per_year) #for exploration
 row.names(source) <- labels
 row.names(df.fatal) <- labels
 
 # hierarchical clustering
-d <- dist(df.fatal, method = "maximum")
+d <- dist(df.fatal, method = "euclidean")
 clust <- hclust(d, "ward.D2")
 groups <- cutree(clust, 4)
 
@@ -20,4 +20,4 @@ palette(value = c('red', 'blue', 'green', 'black'))
 plot(df.fatal, col = as.factor(groups))
 text(df.fatal, labels = row.names(df.fatal),cex=0.7, pos=3)  # create labels
 
-title(main = "Worker Fatality per 100000/year and GDP per Capita")
+title(main = "GDP per Capita and Worker Fatalities per 100000/year")
